38f544b761ce6342969d3e211707ba0ddbf89971
moyun <zero305747648@gmail.com>
2026-05-07 00:23:32 +0800

    feat:增加项目

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,12 @@ const base = import.meta.env.BASE_URL;
  * 项目数据
  */
 export const projects = [
+     {
+        imgSrc: `${base}images/xqmo.png`,
+        title: '心晴MO',
+        tags: ['ReactNative', 'Expo', 'TypeScript'],
+        projectLink: 'https://apps.apple.com/us/app/%E5%BF%83%E6%99%B4mo/id6759703686 '
+    },
     {
         imgSrc: `${base}images/blue-ai.png`,
         title: 'Blueprint AI',
@@ -17,9 +23,10 @@ export const projects = [
         projectLink: 'https://no-money-xiuxian.vercel.app/'
     },
     {
-        imgSrc: `${base}images/xqmo.png`,
-        title: '心晴MO',
-        tags: ['ReactNative', 'Expo', 'TypeScript'],
-        projectLink: 'https://apps.apple.com/us/app/%E5%BF%83%E6%99%B4mo/id6759703686 '
+        imgSrc: `${base}images/travel.png`,
+        title: '旅行记录地图',
+        tags: ['Nuxt', 'Vue3', 'Pinia'],
+        projectLink: 'https://travel-record-map.vercel.app/'
     }
+   
 ];
